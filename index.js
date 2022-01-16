767040c172ad658848d27f579b4c553fe22acfca
Yordy2001 <kingyordi2001@gmail.com>
2022-01-16 19:38:09 -0400

    install dependece

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+
+
+app.get('/', (req, res) => {
+    res.send('chat')
+})
+
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+})
