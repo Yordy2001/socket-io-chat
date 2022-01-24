f1d60e1f007e3324a38fad1266f9e5fcfb716f3e
Yordy2001 <kingyordi2001@gmail.com>
2022-01-23 20:32:03 -0400

    show message

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ app.get('/', (req, res) => {
 })
 
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-    });
-});
-
 // io.on('connection', (socket) => {
 //     socket.on('chat message', (msg) => {
-//       io.emit('chat message', msg);
+//       console.log('message: ' + msg);
 //     });
 // });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
