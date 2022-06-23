1d7d5c973a2a267af2058513668ed6a73102b86f
Yordy2001 <kingyordi2001@gmail.com>
2022-06-23 11:36:18 -0400

    fix register, login, add body parser parser, bycript, fix structure

--- server/app.js
+++ server/app.js
@@ -1,42 +1,33 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
+const router = require('./routers/user.routes')
 //Server config 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const { User } = require('./db/index')
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use('/static', express.static('public'))
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
+app.use(router)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-
 io.on('connection', (socket) => {
     // socket.on('chat-message', (msg) => {
     //     io.emit('chat-message', msg);
     // });
-
-    socket.on('client:register', async (register) => {
-        const {name, tel, portada, info} = register
-    
-        try {
-            await User.create({
-                full_name: name,
-                tel,
-                portada,
-                info
-            })
-
-        } catch (error) {
-            console.log(error)
-        }
-    });
 
 });
 
