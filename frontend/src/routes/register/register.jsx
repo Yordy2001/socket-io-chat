211b396b1791e8e1f5f44cc63cc20de9a74f18d1
Yordy2001 <kingyordi2001@gmail.com>
2022-06-30 15:29:33 -0400

    enable cors

--- frontend/src/routes/register/register.jsx
+++ frontend/src/routes/register/register.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import axios from 'axios'
 
 import '../../App.css'
 import './register.css'
+import { useState } from 'react'
+
+import portada from '../../assets/img/avatar.svg' 
 
 export default function Register() {
 
+    const [first, setFirst] = useState(true)
     const formik = useFormik({
         initialValues: {
             tel: '',
@@ -17,50 +22,62 @@ export default function Register() {
             await axios.post('http://localhost:4000/register', values)
         },
     })
+    const handleForm = () => {
+        setFirst(!first)
+    }
+
 
     return (
         <div className='register-page'>
             <div className='form-content'>
                 <h1>Register</h1>
                 <form onSubmit={formik.handleSubmit}>
-                    <input
+                    {
+                        (first) ?
 
-                        type="tel"
-                        name="tel"
-                        placeholder='Phone'
-                        required
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                    />
+                            <>
+                                <input
 
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder='Name'
-                        required
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                    />
+                                    type="tel"
+                                    name="tel"
+                                    placeholder='Phone'
+                                    required
+                                    onChange={formik.handleChange}
+                                    value={formik.values.email} />
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder='Name'
+                                    required
+                                    onChange={formik.handleChange}
+                                    value={formik.values.email} />
 
-                    {/* <input
-                        id="portada"
-                        type="file"
-                        name="portada"
-                        placeholder='Portada'
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                    />
-
-                    <textarea
-                        id="info"
-                        type="text"
-                        name="info"
-                        placeholder='Info'
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                    /> */}
-
-                    <button type='submit' className='btn-next'>Next</button>
+                                <button className='btn-next' onClick={handleForm}>Next</button>
+                            </>
+                            :
+                            <>
+                                <label htmlFor="portada" className='lbl-portada'></label>
+                                <input
+                                    id="portada"
+                                    type="file"
+                                    name="portada"
+                                    placeholder='Portada'
+                                    className='input-file'
+                                    onChange={formik.handleChange}
+                                    value={formik.values.email} />
+                                <textarea
+                                    id="info"
+                                    type="text"
+                                    name="info"
+                                    placeholder='Info'
+                                    onChange={formik.handleChange}
+                                    value={formik.values.email} />
+                            
+                                <button onClick={handleForm}>opmit</button>
+                                <button type='submit' className='btn-next' >Send</button>
+                            </>            
+                    }
+                    
                 </form>
             </div>
         </div>
